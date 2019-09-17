f0a391b1a54d0c2d688f9e742792a1709db9f9d2
Lavitor Matzembacker <51544066+lavitorm@users.noreply.github.com>
2019-09-17 16:00:35 -0700

    database, app and json

---
+++ bamazon.sql
@@ -0,0 +1,30 @@
+/*
+1. Create a MySQL Database called `bamazon`.
+2. Then create a Table inside of that database called `products`.
+3. The products table should have each of the following columns:
+   * item_id (unique id for each product)
+   * product_name (Name of product)
+   * department_name
+   * price (cost to customer)
+   * stock_quantity (how much of the product is available in stores)
+4. Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+
+SHOW DATABASES;
+*/
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+/* connect to the bamazon database*/
+USE bamazon_db;
+CREATE TABLE products (
+  item_id INT (10) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT (100) NOT NULL,
+  PRIMARY KEY (item_id)
+);
+SELECT * FROM products;
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES ("shoes", "kids", 99.99, 5), 
+("hat", "women", 69.99, 6), ("ball", "sports", 24.00, 8), ("pants", "men", 79.99, 16), ("shorts", "kids", 35.00, 10),
+("TV", "eletronics", 399.99, 20), ("computer", "eletronics", 499.99, 11), ("rug", "home", 109.99, 2), 
+("sweater", "women", 39.99, 8), ("coat", "men", 199.99, 10);
---
+++ bamazonCustomer.js
@@ -0,0 +1,81 @@
+/*
+5. create a Node application called `bamazonCustomer.js`. Running this application will first display 
+all of the items available for sale. Include the ids, names, and prices of products for sale.
+6. The app should then prompt users with two messages.
+    * The first should ask them the ID of the product they would like to buy.
+    * The second message should ask how many units of the product they would like to buy.
+7. Once the customer has placed the order, your application should check if your store has enough of the product 
+to meet the customer's request.
+    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+    * This means updating the SQL database to reflect the remaining quantity.
+    * Once the update goes through, show the customer the total cost of their purchase.
+*/
+var mysql = require('mysql');
+// Load the NPM Package inquirer
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+// mysql -u root -p
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'bamazon_db',
+    port: 3306
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
+});
+var displayProducts = function () {
+    var query = "Select * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        var displayTable = new Table({
+            head: ["Item ID", "Product Name", "Catergory", "Price", "Quantity"],
+            colWidths: [10, 25, 25, 10, 14]
+        });
+        for (var i = 0; i < res.length; i++) {
+            displayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(displayTable.toString());
+        purchasePrompt();
+    });
+}
+function purchasePrompt() {
+    inquirer.prompt([
+        {
+            name: "ID",
+            type: "input",
+            message: "Please enter the Item ID of the product you'd like to buy",
+            filter: Number
+        },
+        {
+            name: "Quantity",
+            type: "input",
+            message: "How many units of this product would you like to buy?",
+            filter: Number
+        },
+
+    ]).then(function (answers) {
+        var quantityNeeded = answers.Quantity;
+        var IDrequested = answers.ID;
+        purchaseOrder(IDrequested, quantityNeeded);
+    });
+};
+function purchaseOrder(ID, amtNeeded) {
+    connection.query('Select * FROM products WHERE item_id = ' + ID, function (err, res) {
+        if (err) { console.log(err) };
+        if (amtNeeded <= res[0].stock_quantity) {
+            var totalCost = res[0].price * amtNeeded;
+            console.log("Your total cost for " + amtNeeded + " " + res[0].product_name + " is " + totalCost + " Thank you!");
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
+        } else {
+            console.log("Insufficient quantity!");
+        };
+        displayProducts();
+    });
+};
+displayProducts(); 
